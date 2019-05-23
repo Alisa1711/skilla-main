87e54e281c98a3228d1fa2946fa1610c2fe6416d
Alisa1711 <alisa.sp@gmail.com>
2019-05-24 02:03:14 +0300

    add main

--- src/blocks/main-hero/main-hero.js
+++
@@ -1,7 +0,0 @@
-$(window).on(`load resize`, () => {
-  if (window.matchMedia(mediaQuery.large).matches) {
-    $(`.main-hero__inner`).append($(`.main-hero__top`));
-  } else {
-    $(`.main-hero`).append($(`.main-hero__top`));
-  }
-});
