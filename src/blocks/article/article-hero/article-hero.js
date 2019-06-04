cc0fcf91472eb9c272d604f62ef0ac488f9cf11c
 Apple iMac <iMac@iMac.local>
2019-06-04 13:48:40 +0300

    add article-hero blocks

--- src/blocks/article/article-hero/article-hero.js
+++ src/blocks/article/article-hero/article-hero.js
@@ -1,14 +1,21 @@
-$(window).on(`load resize`, () => {
-  let image = `${$(`.article-hero__img`).attr(`src`)}`;
-  let overlay = `linear-gradient(rgba(138, 70, 198, 0.80) 0%, rgba(235, 87, 87, 0.5) 100%), linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5))`;
+(function () {
+  const setArticleBg = () => {
+    let image = $(`.article-hero__img:hidden`);
+    let imageSrc = image.attr(`src`);
+    let overlay = `linear-gradient(rgba(138, 70, 198, 0.80) 0%, rgba(235, 87, 87, 0.5) 100%), linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5))`;
 
-  if (window.matchMedia(mediaQuery.large).matches) {
-    $(`.article-hero`).css({'backgroundImage': `${overlay}, url("${image}")`});
-    $(`.article-hero--main .container`).append($(`.top-posts`));
-  } else if (window.matchMedia(mediaQuery.medium).matches) {
-    $(`.article-hero`).css({'backgroundImage': `${overlay}, url("${image}")`});
-    $(`.article-hero--main`).after($(`.top-posts`));
-  } else {
-    $(`.article-hero`).css({'backgroundImage': ``});
-  }
-});
+    if ($(window).width() >= 1200) {
+      $(`.article-hero`).css({'backgroundImage': `${overlay}, url("${imageSrc}")`});
+      $(`.article-hero--main .container`).append($(`.top-posts`));
+    } else if ($(window).width() >= 768) {
+      $(`.article-hero`).css({'backgroundImage': `${overlay}, url("${imageSrc}")`});
+      $(`.article-hero--main`).after($(`.top-posts`));
+    } else {
+      $(`.article-hero`).css({'backgroundImage': ``});
+    }
+  };
+
+  $(window).on(`load resize`, () => {
+    setArticleBg();
+  });
+})();
